c4c22e54a4d6fa20ed3a13d35ab9e6e2d96b0e90
kalpanadd <dkdevi.dkd@gmail.com>
2021-10-16 21:44:04 +0530

    styling for current component done

--- src/COMPONENTS/ForecastWeather/Forecast.jsx
+++ src/COMPONENTS/ForecastWeather/Forecast.jsx
@@ -10,6 +10,7 @@ class Forecast extends React.Component {
                 url: `http://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`,
                 alt: `Image of  ${f.weather[0].description}`,
             };
+            const feels_like = f.feels_like;
             const description = f.weather[0].description;
             const unixTimestamp = f.dt;
             let hour = new Date(unixTimestamp * 1000).getHours();
@@ -21,13 +22,30 @@ class Forecast extends React.Component {
             }
 
             return (
-                <div key={i} className="forecast-item">
-                    <p className="forecast-item__time">{hour} {ampm}</p>
-                    <p className="forecast-item__temp">
-                        {f.temp} <span className="forecast-item__degree">°</span>
-                    </p>
-                    <img className="forecast-item__img" src={image.url} alt={image.alt} />
-                    <p className="forecast-item__description">{description}</p>
+                // <div key={i} className="forecast-item">
+                //     <p className="forecast-item__time">{hour} {ampm}</p>
+                //     <p className="forecast-item__temp">
+                //         {f.temp} <span className="forecast-item__degree">°</span>
+                //     </p>
+                //     <img className="forecast-item__img" src={image.url} alt={image.alt} />
+                //     <p className="forecast-item__description">{description}</p>
+                // </div>
+
+
+
+                <div className='forecast_div'>
+                    <div className='forecast_card'>
+                        <img className="forecast-item__img" src={image.url} alt={image.alt} />
+                        <p className="forecast-item__time">{hour} {ampm}</p>
+                        <p className="forecast-item__temp">
+                            {f.temp} <span className="forecast-item__degree">&#8451;</span>
+                        </p>
+                        <div>
+                            <p className="forecast-item__description">{description}</p>
+                            <p>FEELS LIKE:{feels_like}&#8451;</p>
+                        </div>
+                    </div>
+
                 </div>
             );
         });
